f0e604d440ccd584a4f3ecb6602d46229ace0eda
송승혁 <tmdgur328@naver.com>
2025-12-19 20:03:54 +0900

    refactor: 주석 추가

--- src/main/java/com/song/project/entity/Review.java
+++ src/main/java/com/song/project/entity/Review.java
@@ -14,23 +14,25 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String content;
-    private int rating;
+
+    private String content;    // 리뷰 내용
+
+    private int rating;    // 리뷰 평점
     
     // 여러 Review는 하나의 User(작성자)를 가리킴
     // Review.reviewer 외래키로 User 참조
     @ManyToOne
     @JoinColumn(name = "reviewer",
             foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private User reviewer;
+    private User reviewer;    // 리뷰 작성자
 
     // 여러 Review는 하나의 User(대상자)를 가리킴
     // Review.targetUser 외래키로 User 참조
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "target_user_id",
             foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private User targetUser;
+    private User targetUser;    // 리뷰 대상자
 
     @CreationTimestamp
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt;    // 리뷰 작성일시
 }
