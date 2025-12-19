255c0189a5f33bd84a6d363623cd594373b394c0
송승혁 <tmdgur328@naver.com>
2025-12-19 20:03:24 +0900

    refactor: 주석 추가

--- src/main/java/com/song/project/entity/PostStatus.java
+++ src/main/java/com/song/project/entity/PostStatus.java
@@ -7,10 +7,12 @@ public enum PostStatus {
 
     private final String description;
 
+    // 게시물 상태 설명
     PostStatus(String description) {
         this.description = description;
     }
 
+    // 게시물 상태 설명 반환
     public String getDescription() {
         return description;
     }
