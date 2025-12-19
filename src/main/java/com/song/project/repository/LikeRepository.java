b68eec8c1b195d70b47e1ae4016ea3f296d2bbf5
송승혁 <tmdgur328@naver.com>
2025-12-19 20:06:10 +0900

    refactor: 주석 추가

--- src/main/java/com/song/project/repository/LikeRepository.java
+++ src/main/java/com/song/project/repository/LikeRepository.java
@@ -16,18 +16,22 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Likes, Long> {
+    // 사용자와 게시물 조회
     Optional<Likes> findByUserAndPost(User user, Post post);
 
+    // 사용자 ID로 좋아요 목록 조회
     @EntityGraph(attributePaths = {"post"})
     List<Likes> findByUserId(Long id);
 
+    // 사용자 ID로 좋아요 목록 페이지 조회
     @EntityGraph(attributePaths = {"post"})
     Page<Likes> findByUserId(Long id, Pageable pageable);
 
+    // 게시물 ID로 좋아요 목록 조회
     @EntityGraph(attributePaths = {"post"})
     List<Likes> findByPostId(Long postId);
 
-    // postId로 모든 좋아요 삭제
+    // 게시물 ID로 모든 좋아요 삭제
     @Modifying
     @Query("DELETE FROM Likes l WHERE l.post.id = :postId")
     void deleteAllByPostId(@Param("postId") Long postId);
