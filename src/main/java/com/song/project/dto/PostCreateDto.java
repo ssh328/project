0f3e356b3e2fcc178f6004f4aeda45d1efbec714
송승혁 <tmdgur328@naver.com>
2025-12-19 19:07:10 +0900

    refactor: PostCreateDto 주석 추가

--- src/main/java/com/song/project/dto/PostCreateDto.java
+++ src/main/java/com/song/project/dto/PostCreateDto.java
@@ -9,19 +9,19 @@ import lombok.Setter;
 public class PostCreateDto {
     @NotBlank(message = "제목을 입력해주세요.")
     @Size(min = 2, max = 250, message = "제목은 2자 이상 250자 이하여야 합니다.")
-    private String title;
+    private String title;   // 게시물 제목
 
     @NotNull(message = "가격을 입력해주세요.")
     @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
     @Max(value = 999999999, message = "가격은 999,999,999원 이하여야 합니다.")
-    private Integer price;
+    private Integer price;   // 게시물 가격
 
     @NotBlank(message = "카테고리를 선택해주세요.")
-    private String category;
+    private String category;   // 게시물 카테고리
 
     @NotBlank(message = "내용을 입력해주세요.")
     @Size(min = 10, max = 10000, message = "내용은 10자 이상 10,000자 이하여야 합니다.")
-    private String body;
+    private String body;   // 게시물 내용
 
-    private String image; // 선택적 필드
+    private String image;   // 게시물 이미지
 }
