2484585daf458370312c6392670dd36f6f77ee8c
송승혁 <tmdgur328@naver.com>
2025-12-19 19:59:24 +0900

    refactor: 주석 추가

--- src/main/java/com/song/project/entity/Chat.java
+++ src/main/java/com/song/project/entity/Chat.java
@@ -13,12 +13,16 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @Column(name = "name")
-    private String name;
+    private String name;    // 사용자 이름
+
     @Column(name = "dp")
-    private String dp;
+    private String dp;    // 사용자 프로필 이미지
+
     @Column(name = "email")
-    private String email;
+    private String email;    // 사용자 이메일
+
     @Column(name = "role")
-    private String role;
+    private String role;    // 사용자 역할
 }
